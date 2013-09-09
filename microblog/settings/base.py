5048663fd09e0c8aafb955f40244709f82511011
Eric Engmann <b3ngmann@gmail.com>
2013-09-09 23:26:17 +0000

    updates to settings.py and turned on admin in urls.py

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = root("..", "uploads"),
+MEDIA_ROOT = root("..", "uploads")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root("..", "static"),
+STATIC_ROOT = root("..", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -126,7 +126,6 @@ DJANGO_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    # 'django.contrib.admindocs',
 )
 
 THIRD_PARTY_APPS = (
